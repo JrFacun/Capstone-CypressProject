836c36406a15ddacebb216399b81a195fc32e20c
joe-b-reaker <josericardo.c.ayala@gmail.com>
2025-08-08 13:30:37 +0800

    Completed Test Scripts for Contact Us

--- cypress/e2e/features/ContactUs.cy.js
+++ cypress/e2e/features/ContactUs.cy.js
@@ -84,4 +84,77 @@ describe("Contact Us Test Cases", () => {
         cy.get(contactUsPage.homeButton).click();
         cy.url().should('eq', 'https://www.automationexercise.com/');
     });
+
+    it("User is able to submit the feedback without an attached file", () => {
+        cy.fixture('contactUs').then((user) => {
+            contactUsPage.fillUpForm(user[0].Name, user[0].Email, user[0].Subject, user[0].Message);
+        })
+        cy.get(contactUsPage.successMessage).should('contain', 'Success! Your details have been submitted successfully.');
+        cy.get(contactUsPage.homeButton).should('exist');
+        cy.get(contactUsPage.homeButton).click();
+        cy.url().should('eq', 'https://www.automationexercise.com/');
+    });
+
+    it("User is not able to submit feedback when only the name is populated", () => {
+        cy.fixture('contactUs').then((user) => {
+            contactUsPage.fillUpForm(user[0].Name, "", "", "");
+        })
+        const fields = [contactUsPage.emailText, contactUsPage.subjectText, contactUsPage.messageText];
+        cy.get(contactUsPage.successMessage).should('not.contain', 'Success! Your details have been submitted successfully.');
+        cy.get(contactUsPage.successMessage).should('not.be.visible');
+        cy.get(contactUsPage.homeButton).should('not.exist');
+
+    });
+
+
+    it("User is not able to submit feedback when only the email is populated", () => {
+        cy.fixture('contactUs').then((user) => {
+            contactUsPage.fillUpForm("", user[0].Email, "",);
+        })
+        cy.get(contactUsPage.successMessage).should('not.contain', 'Success! Your details have been submitted successfully.');
+        cy.get(contactUsPage.successMessage).should('not.be.visible');
+        cy.get(contactUsPage.homeButton).should('not.exist');
+
+    });
+
+    it("User is not able to submit feedback when only the subject is populated", () => {
+        cy.fixture('contactUs').then((user) => {
+            contactUsPage.fillUpForm("", "", user[0].Subject, "");
+        })
+        cy.get(contactUsPage.successMessage).should('not.contain', 'Success! Your details have been submitted successfully.');
+        cy.get(contactUsPage.successMessage).should('not.be.visible');
+        cy.get(contactUsPage.homeButton).should('not.exist');
+
+    });
+
+    it("User is not able to submit feedback when only the message body is populated", () => {
+        cy.fixture('contactUs').then((user) => {
+            contactUsPage.fillUpForm("", "", "", user[0].Message);
+        })
+        cy.get(contactUsPage.successMessage).should('not.contain', 'Success! Your details have been submitted successfully.');
+        cy.get(contactUsPage.successMessage).should('not.be.visible');
+        cy.get(contactUsPage.homeButton).should('not.exist');
+
+    });
+
+    it("User is not able to submit feedback when only the upload file is populated", () => {
+        const fileName = contactUsPage.txtFileName;
+        cy.get(contactUsPage.uploadButton).attachFile(fileName);
+        cy.get(contactUsPage.successMessage).should('not.contain', 'Success! Your details have been submitted successfully.');
+        cy.get(contactUsPage.successMessage).should('not.be.visible');
+        cy.get(contactUsPage.homeButton).should('not.exist');
+    });
+
+    it("User is not able to submit feedback when the uploaded file type is not valid", () => {
+        const fileName = contactUsPage.pptxFileName;
+        cy.get(contactUsPage.uploadButton).attachFile(fileName);
+        cy.get(contactUsPage.successMessage).should('not.contain', 'Success! Your details have been submitted successfully.');
+        cy.get(contactUsPage.successMessage).should('not.be.visible');
+        cy.get(contactUsPage.homeButton).should('not.exist');
+    });
+
+    
+
+
+
 })
